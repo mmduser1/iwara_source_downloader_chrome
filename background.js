5f530df9674282b47a1d96ed2d001d726bb2f4ba
mmduser1 <dummy>
2017-04-16 07:02:57 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,61 @@
+/*
+アイコンがクリックされたときにdown.jsへメッセージを送信する
+*/
+chrome.pageAction.onClicked.addListener((tab) => {
+  chrome.tabs.sendMessage(tab.id, {"download": "run"});
+});
+
+/*
+down.jsから動画情報を受け取ってダウンロードを開始する
+*/
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+
+    chrome.storage.local.get("filename",((settings)=>{
+
+      var filename = request.username + ' - ' + request.title + '.mp4';
+      if (typeof settings.filename !== "undefined") {
+        if(settings.filename.indexOf("type1") != -1){
+          filename = request.username + ' - ' + request.title + '.mp4';
+        } else {
+          filename = '['+request.username+'] ' + request.title + '.mp4';
+        }
+      }
+
+      function onStartedDownload(id) {
+        //console.log("Started to download: "+id);
+      }
+
+      function onFailed(error) {
+        //console.log("Something stinks: "+error);
+      }
+
+      var startDownload = chrome.downloads.download({
+        url : request.source_url,
+        filename: filename,
+        conflictAction : 'prompt',
+        saveAs : true
+      });
+
+      //startDownload.then(onStartedDownload, onFailed);
+    }));
+});
+
+/* エラーログ */
+function onError(e) {
+  console.error(e);
+}
+
+/*
+タブのURLにiwara.tv/videos/があればアイコンを表示
+*/
+function checkForValidUrl(tabId, changeInfo, tab) {
+  if (tab.url.indexOf("iwara.tv/videos/") > -1) {
+    chrome.pageAction.show(tabId);
+  }
+}
+
+/*
+タブの切り替えを捕捉
+*/
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
---
+++ content_scripts/down.js
@@ -0,0 +1,57 @@
+/*
+動画の情報を取得してbackgroundへ投げる
+取得する情報：
+  ・SourceのURL
+  ・タイトル
+  ・アップロードしたユーザーの名前
+*/
+function getInfo(){
+  var button = document.getElementById("download-button");
+  button.click();
+  var links = document.getElementById("download-options").getElementsByTagName('a');
+  var info = document.getElementsByClassName('node-info')[0];
+  var title = info.getElementsByTagName('h1');
+  var username = info.getElementsByTagName('a');
+
+  chrome.runtime.sendMessage({
+    source_url: links[0].href,
+    title: convertSafeFileName(title[0].innerHTML),
+    username: convertSafeFileName(username[1].innerHTML)
+  });
+}
+
+/*
+backgroundからのメッセージを受信したらgetInfoを実行
+*/
+chrome.runtime.onMessage.addListener(getInfo);
+
+/*
+使用できない文字を全角に置き換え
+¥　/　:　*　?　"　<　>　|
+chromeのみ半角チルダを全角チルダへ変換
+*/
+function convertSafeFileName(titleOrUsername){
+  return unEscapeHTML(titleOrUsername)
+    .replace(/\\/g,'￥')
+    .replace(/\//g,'／')
+    .replace(/:/g,'：')
+    .replace(/\?/g,'？')
+    .replace(/"/g,'”')
+    .replace(/</g,'＜')
+    .replace(/>/g,'＞')
+    .replace(/\|/g,'｜').replace(/~/g,'～');
+}
+
+/**
+ * HTMLアンエスケープ
+ *
+ * @param {String} str 変換したい文字列
+ */
+var unEscapeHTML = function (str) {
+    return str
+            .replace(/(&lt;)/g, '<')
+            .replace(/(&gt;)/g, '>')
+            .replace(/(&quot;)/g, '"')
+            .replace(/(&#39;)/g, "'")
+            .replace(/(&amp;)/g, '&');
+};
